75b4df157088d47b1b392091850ca45a13508c17
Alex Contreras <alexconruiz@gmail.com>
2024-02-12 20:27:43 -0300

    FEAT[ROLLUP]: Tried other configs

--- modules/Card/rollup.config.js
+++ modules/Card/rollup.config.js
@@ -2,12 +2,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
+
 import postcss from 'rollup-plugin-postcss';
 import terser from '@rollup/plugin-terser';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-
-// const packageJson = require('./package.json');
 import packageJson from  './package.json' assert { type: 'json' };
 
 export default [
@@ -17,21 +16,21 @@ export default [
 			{
 				file: packageJson.main,
 				format: 'cjs',
-				sourcemap: false,
+				sourcemap: true,
 			},
 			{
 				file: packageJson.module,
 				format: 'esm',
-				sourcemap: false,
+				sourcemap: true,
 			},
 		],
 		plugins: [
+			typescript({ tsconfig: './tsconfig.json' }),
 			peerDepsExternal(),
 			resolve(),
 			commonjs(),
-			typescript({ tsconfig: './tsconfig.json' }),
-			postcss(),
 			terser(),
+			postcss(),
 		],
 	},
 	{
